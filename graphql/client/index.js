369dbfb2d8f6f883b6327b3016ca14a1533b8da9
Rafael de Moura Speroni <rafaelsperoni@gmail.com>
2023-11-14 20:13:34 +0000

    client

--- graphql/client/index.js
+++ graphql/client/index.js
@@ -1,7 +1,15 @@
-fetch('http://localhost:4000', {
+fetch('https://refactored-space-guacamole-97jj57qxpjf7jr-4000.app.github.dev/', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ query: '{cursos { id, nome } }' }),
+  body: JSON.stringify({ query: `
+  {
+    cursos {
+      nome
+      coordenador {
+        nome
+      }
+    }
+  }` }),
 })
   .then(res => res.json())
   .then(res => console.log(res.data));
